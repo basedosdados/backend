ed4da9dca9c85530fd8b6ba6a1bca490793bcc55
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-11-03 11:03:48 -0300

    feat(alembic): add migrations to `run.sh` script

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ export APP_MODULE=${APP_MODULE-app.main:app}
 export HOST=${API_HOST:-0.0.0.0}
 export PORT=${API_PORT:-8001}
 
+# Run migrations
+echo "Running migrations..."
+alembic upgrade head
+
 exec uvicorn --host $HOST --port $PORT "$APP_MODULE"
