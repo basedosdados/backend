6c3657473703523aaa90f827acce5c7148992db6
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-11-22 09:46:39 -0300

    chore: refactor for django

---
+++ scripts/lint.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from glob import glob
+import subprocess
+
+
+def main():
+    """
+    Lint all python files in the project.
+    """
+    files = glob("**/*.py", recursive=True)
+    subprocess.run(["flake8", *files])
