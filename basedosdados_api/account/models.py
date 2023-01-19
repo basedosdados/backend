5425d674cdcff3494d501e04b1fe714a10e2d009
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-01-19 14:42:30 -0300

    feat: extend user model with `Profile`

--- basedosdados_api/account/models.py
+++ basedosdados_api/account/models.py
@@ -2,6 +2,9 @@
 from uuid import uuid4
 
 from django.db import models
+from django.contrib.auth.models import User
+
+from basedosdados_api.api.v1.models import Organization
 
 
 class RegistrationToken(models.Model):
@@ -16,3 +19,20 @@ class RegistrationToken(models.Model):
     class Meta:
         verbose_name = "Registration Token"
         verbose_name_plural = "Registration Tokens"
+
+
+class Profile(models.Model):
+    # Foreign
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organizations = models.ManyToManyField(
+        Organization, related_name="users", blank=True
+    )
+    # Mandatory
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255)
+    # Optional
+    birth_date = models.DateField(null=True, blank=True)
+    picture_url = models.URLField(null=True, blank=True)
+    twitter = models.CharField(max_length=255, null=True, blank=True)
+    linkedin = models.CharField(max_length=255, null=True, blank=True)
