868fa9c35a459684b82879b0db5ba665786db468
d116626 <d116626@gmail.com>
2023-02-22 19:06:56 -0300

    chore: migrate information request

--- utils/migration/2023-01-19-ckan-django.py
+++ utils/migration/2023-01-19-ckan-django.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import re
+from datetime import datetime
 
 j = json.load(open("./credentials.json"))
 USERNAME = j["username"]
@@ -120,7 +121,6 @@ class Migration:
             ).json()
 
             if "data" in r:
-                print("HEREEEEEEEEEEEE")
                 if r["data"][mutation_class]["errors"] != []:
                     print(f"not found {mutation_class}", mutation_parameters)
                     id = None
@@ -176,22 +176,6 @@ class Migration:
             ids.append(id)
         return ids
 
-    def create_themes(self, objs):
-        ids = []
-        for obj in objs:
-            r, id = self.create_update(
-                mutation_class="CreateUpdateTheme",
-                mutation_parameters={
-                    "slug": obj.get("name"),
-                    "name": obj.get("title"),
-                    "logoUrl": obj.get("image_display_url"),
-                },
-                query_class="allTheme",
-                query_parameters={"$slug: String": obj.get("name")},
-            )
-            ids.append(id)
-        return ids
-
     def create_tags(self, objs):
         ids = []
         for obj in objs:
@@ -216,6 +200,46 @@ class Migration:
             },
             query_class="allAvailability",
             query_parameters={"$slug: String": obj.get("availability")},
+        )
+
+        return id
+
+    def create_entity(self):
+        r, id = self.create_update(
+            mutation_class="CreateUpdateEntity",
+            mutation_parameters={
+                "slug": "desconhecida",
+                "name": "todo",
+            },
+            query_class="allEntity",
+            query_parameters={"$slug: String": "desconhecida"},
+        )
+
+        return id
+
+    def create_update_frequency(self, entity_id):
+        r, id = self.create_update(
+            mutation_class="CreateUpdateUpdateFrequency",
+            mutation_parameters={
+                "entity": entity_id,
+                "number": 0,
+            },
+            query_class="allUpdatefrequency",
+            query_parameters={"$number: Int": 0},
+        )
+
+        return id
+
+    def create_license(self):
+        r, id = self.create_update(
+            mutation_class="CreateUpdateLicense",
+            mutation_parameters={
+                "slug": "desconhecida",
+                "name": "todo",
+                "url": "todo.com",
+            },
+            query_class="allLicense",
+            query_parameters={"$slug: String": "desconhecida"},
         )
 
         return id
@@ -246,56 +270,68 @@ if __name__ == "__main__":
     # packages = get_bd_packages()
     package = get_package_model()
     m = Migration(TOKEN)
-
+    entity_id = m.create_entity()
+    update_frequency_id = m.create_update_frequency(entity_id)
     # r = m.delete(classe="Dataset", id="77239376-6662-4d64-8950-2f57f1225e53")
 
     for p in [package]:
-        # create tags
-        tags_ids = m.create_tags(objs=p.get("tags"))
-        themes_ids = m.create_themes(objs=p.get("groups"))
+        #     # create tags
+        #     tags_ids = m.create_tags(objs=p.get("tags"))
+        #     themes_ids = m.create_themes(objs=p.get("groups"))
 
-        ## create organization
-        org_slug = p.get("organization").get("name").replace("-", "_")
-        package_to_org = {
-            "area": m.create_update(
-                query_class="allArea",
-                query_parameters={"$slug: String": "desconhecida"},
-                mutation_class="CreateUpdateArea",
-                mutation_parameters={"slug": "desconhecida"},
-            )[1],
-            "slug": org_slug,
-            "name": p.get("organization").get("title"),
-            "description": p.get("organization").get("description"),
-        }
-        r, org_id = m.create_update(
-            query_class="allOrganization",
-            query_parameters={"$slug: String": org_slug},
-            mutation_class="CreateUpdateOrganization",
-            mutation_parameters=package_to_org,
-        )
+        #     ## create organization
+        #     org_slug = p.get("organization").get("name").replace("-", "_")
+        #     package_to_org = {
+        #         "area": m.create_update(
+        #             query_class="allArea",
+        #             query_parameters={"$slug: String": "desconhecida"},
+        #             mutation_class="CreateUpdateArea",
+        #             mutation_parameters={"slug": "desconhecida"},
+        #         )[1],
+        #         "slug": org_slug,
+        #         "name": p.get("organization").get("title"),
+        #         "description": p.get("organization").get("description"),
+        #     }
+        #     r, org_id = m.create_update(
+        #         query_class="allOrganization",
+        #         query_parameters={"$slug: String": org_slug},
+        #         mutation_class="CreateUpdateOrganization",
+        #         mutation_parameters=package_to_org,
+        #     )
 
-        ## create dataset
-        package_to_dataset = {
-            "organization": org_id,
-            "slug": p["name"].replace("-", "_"),
-            "name": p["title"],
-            "description": p["notes"],
-            "tags": tags_ids,
-            "themes": themes_ids,
-        }
-        r, dataset_id = m.create_update(
-            query_class="allDataset",
-            query_parameters={"$slug: String": p["name"].replace("-", "_")},
-            mutation_class="CreateUpdateDataset",
-            mutation_parameters=package_to_dataset,
-        )
-        print(dataset_id)
-
+        #     ## create dataset
+        #     package_to_dataset = {
+        #         "organization": org_id,
+        #         "slug": p["name"].replace("-", "_"),
+        #         "name": p["title"],
+        #         "description": p["notes"],
+        #         "tags": tags_ids,
+        #         "themes": themes_ids,
+        #     }
+        #     r, dataset_id = m.create_update(
+        #         query_class="allDataset",
+        #         query_parameters={"$slug: String": p["name"].replace("-", "_")},
+        #         mutation_class="CreateUpdateDataset",
+        #         mutation_parameters=package_to_dataset,
+        #     )
+        #     print(dataset_id)
+        dataset_id = "da237d91-b2a4-4193-ae96-395f8bcdd004"
         for resource in p["resources"]:
             resource_type = resource["resource_type"]
 
             if resource_type == "bdm_table":
                 print("  CreateTable | CreateCloudTable")
+
+                resource_to_table = {
+                    "dataset": dataset_id,
+                }
+
+                r, raw_source_id = m.create_update(
+                    mutation_class="CreateUpdateTable",
+                    mutation_parameters=resource_to_table,
+                    query_class="allTable",
+                    query_parameters={"$slug: String": resource["table_id"]},
+                )
                 if "columns" in resource:
                     print("    CreateColumn")
 
@@ -304,11 +340,11 @@ if __name__ == "__main__":
                 print("  CreateRawDataSource")
                 resource_to_raw_data_source = {
                     "dataset": dataset_id,
-                    "coverages": "",
+                    # "coverages": "",
                     "availability": m.create_availability(resource),
                     # "languages": "",
-                    # "license": "",
-                    # "updateFrequency": "",
+                    "license": m.create_license(),
+                    "updateFrequency": update_frequency_id,
                     "areaIpAddressRequired": m.create_update(
                         query_class="allArea",
                         query_parameters={"$slug: String": "desconhecida"},
@@ -340,3 +376,35 @@ if __name__ == "__main__":
 
             elif resource_type == "information_request":
                 print("  CreateInformationRequest")
+
+                resource_to_information_request = {
+                    "dataset": dataset_id,
+                    "status": m.create_update(
+                        query_class="allStatus",
+                        query_parameters={"$slug: String": resource["state"]},
+                        mutation_class="CreateUpdateStatus",
+                        mutation_parameters={
+                            "slug": resource["state"],
+                            "name": resource["state"],
+                        },
+                    )[1],
+                    "updateFrequency": update_frequency_id,
+                    "origin": resource["origin"],
+                    "slug": resource["name"],
+                    "url": resource["url"],
+                    "startedAt": datetime.strptime(
+                        resource["opening_date"], "%d/%m/%Y"
+                    ).strftime("%Y-%m-%d")
+                    + "T00:00:00",
+                    "dataUrl": resource["data_url"],
+                    "observations": resource["department"],
+                    "startedBy": 1,
+                    # "observationLevel": "",
+                }
+                r, raw_source_id = m.create_update(
+                    mutation_class="CreateUpdateInformationRequest",
+                    mutation_parameters=resource_to_information_request,
+                    query_class="allInformationrequest",
+                    query_parameters={"$url: String": resource["url"]},
+                )
+                print(r)
