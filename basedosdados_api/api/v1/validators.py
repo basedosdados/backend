501f968f0e768efa1a05338535a09e11d17ca3c9
d116626 <d116626@gmail.com>
2023-03-16 16:05:23 -0300

    fix: change model class name

--- basedosdados_api/api/v1/validators.py
+++ basedosdados_api/api/v1/validators.py
@@ -1,34 +1,4 @@
 # -*- coding: utf-8 -*-
-import re
 import os
-import json
-
-from django.conf import settings
-from django.core.exceptions import ValidationError
 
 current_site = os.getenv("url")
-
-
-def validate_area_key(value):   # outdated because it assumes separators as `.` instead of `_`
-    """Validate area hierarchy."""
-    regex = re.compile(r"^[a-z]+(\.[a-z0-9]+)*$")
-    if not regex.match(value):
-        raise ValidationError(
-            f"Area '{value}' is not valid. \
-            Area must be a dot-separated string of lowercase letters \
-            or numbers (except for the first part, that must be letters)."
-        )
-
-
-def validate_is_valid_area_key(value):  # outdated because it assumes separators as `.` instead of `_`
-    """Validate area key is in BD dict."""
-    with open(
-        settings.BASE_DIR / "schemas/repository/bd_spatial_coverage_tree.json"
-    ) as f:
-        area_key_dict = json.load(f)
-        area_key_dict = area_key_dict.get("result")
-    if value not in area_key_dict.keys():
-        raise ValidationError(
-            f"Area '{value}' is not valid. Area must valid.\
-            See {current_site}/schemas/bd_spatial_coverage_tree/ for valid areas."
-        )
