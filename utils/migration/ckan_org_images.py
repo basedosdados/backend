e0b6275d09f2988e065cde36f244276614290a9b
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2023-04-06 22:04:00 -0300

    image upload

--- utils/migration/ckan_org_images.py
+++ utils/migration/ckan_org_images.py
@@ -1,7 +1,12 @@
 from datetime import datetime
 
+import json
 import requests
 from pathlib import Path
+
+import urllib3
+
+from utils.migration.ckan_org_utils import load_token, get_url
 
 CKAN_ALL_ORGS_URL = "https://basedosdados.org/api/3/action/organization_list"
 CKAN_ORG_URL = "https://basedosdados.org/api/3/action/organization_show?id={org_id}"
@@ -53,14 +58,14 @@ def rename_image(slug: str, image: str) -> Path:
     else:
         print("Downloading image from org:", slug)
         ext = image.split(".")[-1]
-        if ext not in ["png", "jpg", "jpeg"]:
+        if ext not in ["png", "jpg", "jpeg", "gif", "svg"]:
             ext = "jpg"
         # salvar a imagem com o novo nome
-        r = requests.get(CKAN_IMG_URL.format(img_name=image), timeout=90)
         new_image = f"{slug}.{ext}"
         if (IMG_PATH / new_image).exists():
             print("Image already exists:", new_image)
         else:
+            r = requests.get(CKAN_IMG_URL.format(img_name=image), timeout=90)
             with open(IMG_PATH / new_image, "wb") as f:
                 f.write(r.content)
             print("Saved image:", new_image)
@@ -92,20 +97,77 @@ def get_org_uuid(org: tuple) -> tuple:
         return None, None
 
 
-def upload_image(uuid, image):
-    pass
+def upload_image(uuid, image, slug, mode="local"):
+    token = load_token(mode=mode)
+    url = get_url(mode=mode)
+    picture_fname = image.strip('\n')
+
+    query = f"""
+        mutation ($picture: Upload){{
+            CreateUpdateOrganization(input: {{id: "{uuid}", picture: $picture}}) {{
+                errors {{
+                    field
+                    messages
+                }}
+                organization {{
+                    id
+                    picture
+                }}
+            }}
+        }}
+    """
+    variables = {"picture": None}
+    with open(picture_fname, "rb") as f:
+        picture = f.read()
+
+    files = [
+        ("operations", (None, json.dumps({"query": query, "variables": variables}))),
+        ("map", (None, json.dumps({"0": ["variables.picture"]}))),
+        ("0", (picture_fname, picture, "image/*")),
+    ]
+
+    boundary = "-----011000010111000001101001"
+    body, content_type = urllib3.encode_multipart_formdata(files, boundary=boundary)
+
+    headers = {
+        "Content-Type": content_type,
+        "Authorization": "Bearer " + token,
+    }
+
+    try:
+        response = requests.post(url, data=body, headers=headers)
+        err = response.json().get("data", {}).get("CreateUpdateOrganization", {}).get("errors")
+        print(response.text)
+        print(uuid, image, url)
+
+        if not err:
+            return True, f"{uuid}, {slug}, OK"
+        else:
+            return False, f'{uuid}, {slug}, {err}'
+    except Exception as e:
+        return False, f"{uuid}, {slug}, {e}"
 
 
 def main():
+    mode = "staging"
     if not LOG_PATH.exists():
         LOG_PATH.mkdir()
 
     if (LOG_PATH / "orgs.csv").exists():
         print("Uploading organization images")
         with open(LOG_PATH / "orgs.csv", "r") as f:
+            if (LOG_PATH / "upload_log.csv").exists():
+                (LOG_PATH / "upload_log.csv").unlink()
+
+            # count = 0
             for line in f:
                 uuid, slug, image = line.split(",")
-                uploaded = upload_image(uuid, image)
+                status, msg = upload_image(uuid, image, slug, mode=mode)
+                with open(LOG_PATH / "upload_log.csv", "a") as f:
+                    f.write(f"{msg}\n")
+                # count += 1
+                # if count == 10:
+                #     break
     else:
         print("Downloading CKAN organization images")
         errors = []
@@ -116,8 +178,6 @@ def main():
             (LOG_PATH / "errors.txt").unlink()
 
         found = []
-        with open(LOG_PATH / "orgs.csv", "w") as f:
-            f.write("uuid,slug,image\n")
 
         for slug in all_orgs:
             org_data = get_org_data(slug)
@@ -129,7 +189,6 @@ def main():
                     f.write(f"{slug}\n")
             else:
                 found.append(slug)
-                updated = upload_image(uuid, image)
                 with open(LOG_PATH / "orgs.csv", "a") as f:
                     f.write(f"{uuid},{slug},{image}\n")
 
--- utils/migration/ckan_org_utils.py
+++ utils/migration/ckan_org_utils.py
@@ -4,29 +4,30 @@ import requests
 import json
 
 
-def get_credentials(mode):
-    j = json.load(open("./credentials.json"))
-    return j[mode]["username"], j[mode]["password"], j[mode]["url"]
+def get_credentials(mode="local") -> tuple:
+    with open("./credentials.json", "r") as f:
+        j = json.load(f)
+
+    return j[mode]["url"], j[mode]["email"], j[mode]["password"]
 
 
-def get_token(url, username, password):
+def get_token(url, email, password):
+    query = """
+        mutation tokenAuth($email: String!, $password: String!) {
+            tokenAuth(email: $email, password: $password) {
+                payload,
+                refreshExpiresIn,
+                token
+            }
+        }
+        """
+    variables = {"email": email, "password": password}
     r = requests.post(
         url,
         headers={"Content-Type": "application/json"},
         json={
-            "query": """
-                mutation tokenAuth($username: String!, $password: String!) {
-                    tokenAuth(
-                        username: $username,
-                        password: $password,
-                    ) {
-                        payload,
-                        refreshExpiresIn,
-                        token
-                    }
-                }
-            """,
-            "variables": {"username": username, "password": password},
+            "query": query,
+            "variables": variables,
         },
     )
     r.raise_for_status()
@@ -37,7 +38,7 @@ def refresh_token(token, mode="local") -> dict:
     """
     If token is expired, refresh it
     """
-    username, password, url = get_credentials(mode)
+    url, email, password = get_credentials(mode)
     query = '''
         mutation refreshToken($token: String!) {
             refreshToken(token: $token) {
@@ -57,15 +58,13 @@ def refresh_token(token, mode="local") -> dict:
     r.raise_for_status()
     if "errors" in r.json():
         message = r.json()["errors"][0]["message"]
-        raise Exception(
-            F"{message}. You must login again. "
-        )
+        return get_token(*get_credentials(mode))
     return r.json()["data"]["refreshToken"]["token"]
 
 
 def verify_token(token, mode="local") -> bool:
     """Verify if token is valid using unix timestamp"""
-    username, password, url = get_credentials(mode)
+    url, username, password = get_credentials(mode)
     if token == "":
         return False
     query = '''
@@ -95,15 +94,25 @@ def load_token(mode="local"):
     with open("./credentials.json", "r") as f:
         token = json.load(f)[mode]["token"]
 
-    if verify_token(token, mode):
+    if token and verify_token(token, mode):
         return token
-    elif token is not None:
+    elif token:
         token = refresh_token(token, mode)
     else:
         token = get_token(*get_credentials(mode))
-        with open("./credentials.json", "w") as f:
+        print("New token: ", token)
+        with open("./credentials.json", "r") as f:
             credentials = json.load(f)
+
+        with open("./credentials.json", "w") as f:
             credentials[mode]["token"] = token
             json.dump(credentials, f)
 
     return token
+
+
+def get_url(mode="local"):
+    with open("./credentials.json", "r") as f:
+        url = json.load(f)[mode]["url"]
+
+    return url
