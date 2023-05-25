0215838c5ee941ae22a037b97a1cf05eb90fd157
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2023-05-25 13:39:54 -0300

    changing search_indexes

--- basedosdados_api/api/v1/search_indexes.py
+++ basedosdados_api/api/v1/search_indexes.py
@@ -15,6 +15,7 @@ class DatasetIndex(indexes.SearchIndex, indexes.Indexable):
     organization_description = indexes.CharField(
         model_attr="organization__description", null=True
     )
+    organization_picture = indexes.CharField(model_attr="organization__picture")
     table_slugs = indexes.MultiValueField(model_attr="tables__slug", null=True)
     table_names = indexes.MultiValueField(model_attr="tables__name", null=True)
     table_descriptions = indexes.MultiValueField(
@@ -26,8 +27,10 @@ class DatasetIndex(indexes.SearchIndex, indexes.Indexable):
     column_descriptions = indexes.MultiValueField(
         model_attr="tables__columns__description", null=True
     )
-    themes = indexes.MultiValueField(model_attr="themes__name", null=True)
-    tags = indexes.MultiValueField(model_attr="tags__name", null=True)
+    themes_name = indexes.MultiValueField(model_attr="themes__name", null=True)
+    themes_slug = indexes.MultiValueField(model_attr="themes__slug", null=True)
+    tags_name = indexes.MultiValueField(model_attr="tags__name", null=True)
+    tags_slug = indexes.MultiValueField(model_attr="tags__slug", null=True)
     is_closed = indexes.BooleanField(model_attr="is_closed")
 
     def get_model(self):
