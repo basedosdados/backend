8561e25d2c5aae0d76cce9685bbe9197a5058142
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2023-05-31 12:01:13 -0300

    redirect ckan original urls to django

---
+++ basedosdados_api/core/views/dataset_redirect.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from django.http import HttpResponseRedirect
+
+from django.views import View
+
+from basedosdados_api.api.v1.models import Dataset
+
+
+class DatasetRedirectView(View):
+    """View to redirect old dataset urls."""
+
+    def get(self, request, *args, **kwargs):
+        """Redirect to new dataset url."""
+        dataset_slug = request.GET.get("dataset")
+        dataset_slug_list = dataset_slug.split("-")
+        # O padrão mais recente é <local>_<org>_<dataset>
+        # é preciso verificar os outros padrões antes de redirecionar
+        # para o 404
+        try:
+            slug = dataset_slug_list[2]
+            dataset_id = Dataset.objects.get(slug=slug).pk
+            redirect_url = f"http://basedosdados.org/dataset/{dataset_id}/"
+        except Dataset.DoesNotExist:
+            redirect_url = f"http://basedosdados.org/dataset/{dataset_slug}/"
+
+        return HttpResponseRedirect(redirect_url)
