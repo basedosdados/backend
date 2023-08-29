8fee7ca763b057b3e55be9bcf20058a3a749a600
Vinicius <vncsna@gmail.com>
2023-08-29 10:23:32 -0300

    test: cover password reset endpoints

--- basedosdados_api/account/tests.py
+++ basedosdados_api/account/tests.py
@@ -4,6 +4,8 @@ from unittest.mock import MagicMock, patch
 import pytest
 from django.test.client import Client
 from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 from basedosdados_api.account.models import Account
 
@@ -36,24 +38,60 @@ def test_activate_account_confirmation(mock: MagicMock, client: Client):
         username="john.doe",
         email="john.doe@email.com",
     )
-    #
     uid = mock.call_args[0][1]["uid"]
     token = mock.call_args[0][1]["token"]
-    url = reverse("activate", args=[uid, token])
     #
+    url = reverse("activate", args=[uid, token])
     response = client.post(url)
     assert response.status_code == 200
     assert response.json() == {}
+    #
     account.refresh_from_db()
     assert account.is_active
 
 
 @pytest.mark.django_db
-def test_password_reset_view(client):
-
-    ...
+@patch("basedosdados_api.account.views.EmailMultiAlternatives")
+def test_password_reset_request(mock: MagicMock, client: Client):
+    account = Account.objects.create(
+        username="john.doe",
+        email="john.doe@email.com",
+    )
+    uid = urlsafe_base64_encode(force_bytes(account.pk))
+    #
+    url = reverse("password_reset", args=[uid])
+    response = client.post(url)
+    assert response.status_code == 200
+    assert response.json() == {}
+    #
+    assert mock.call_args[0][0] == "Base dos Dados: Redefinição de Senha"
+    assert mock.call_args[0][3] == ["john.doe@email.com"]
+    assert "reset_password" in mock.mock_calls[1][1][0]
 
 
 @pytest.mark.django_db
-def test_password_reset_confirm_view(client):
-    ...
+@patch("basedosdados_api.account.views.render_to_string")
+@patch("basedosdados_api.account.signals.render_to_string")
+def test_password_reset_confirmation(mock_signal: MagicMock, mock_view: MagicMock, client: Client):
+    # Create account
+    account = Account.objects.create(
+        username="john.doe",
+        email="john.doe@email.com",
+    )
+    password0 = account.password
+    uid = mock_signal.call_args[0][1]["uid"]
+    token = mock_signal.call_args[0][1]["token"]
+    # Request password reset
+    url = reverse("password_reset", args=[uid])
+    response = client.post(url)
+    uid = mock_view.call_args[0][1]["uid"]
+    token = mock_view.call_args[0][1]["token"]
+    # Execute password reset
+    url = reverse("password_reset_confirm", args=[uid, token])
+    response = client.post(url, data={"password": "12345678"}, content_type="application/json")
+    assert response.status_code == 200
+    assert response.json() == {}
+    # Verify password reset
+    account.refresh_from_db()
+    password1 = account.password
+    assert password0 != password1
