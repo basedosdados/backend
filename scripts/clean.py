efb68881cde9ee2639eb4ba9e988b5a576ecc696
Vinicius <vncsna@gmail.com>
2023-09-04 21:42:05 -0300

    fix: update default elasticsearch development url

--- scripts/clean.py
+++ scripts/clean.py
@@ -9,16 +9,16 @@ setup()
 def run():
     """Clean development database restrictions"""
     with connection.cursor() as cursor:
+        print("Cleaning database")
         if "postgres" in settings.DATABASES["default"]["ENGINE"]:
-            print("Cleaning database")
             cursor.execute("TRUNCATE auth_permission")
             cursor.execute("TRUNCATE django_admin_log")
             cursor.execute("TRUNCATE django_content_type")
         if "sqlite" in settings.DATABASES["default"]["ENGINE"]:
-            print("Cleaning database")
             cursor.execute("DELETE FROM auth_permission")
             cursor.execute("DELETE FROM django_admin_log")
             cursor.execute("DELETE FROM django_content_type")
+        print("Finished cleaning database")
 
 
 if __name__ == "__main__":
