99a88b4c5f9b7863c48b1897d0cfc003270863f9
Vinicius <vncsna@gmail.com>
2023-09-24 14:02:10 -0300

    chore: update staging frontend url

--- basedosdados_api/core/views/dataset_redirect.py
+++ basedosdados_api/core/views/dataset_redirect.py
@@ -6,24 +6,25 @@ from django.views import View
 
 from basedosdados_api.api.v1.models import CloudTable, Dataset
 
+URL_MAPPING = {
+    "localhost:8001": "localhost:3000",
+    "api.basedosdados.org": "basedosdados.org",
+    "staging.api.basedosdados.org": "staging.basedosdados.org",
+    "development.api.basedosdados.org": "development.basedosdados.org",
+}
+
 
 class DatasetRedirectView(View):
     """View to redirect old dataset urls."""
 
     def get(self, request, *args, **kwargs):
         """Redirect to new dataset url."""
-        full_url = request.build_absolute_uri()  # noqa
+        full_url = request.build_absolute_uri()
         domain = urlparse(full_url).netloc
 
         dataset = request.GET.get("dataset")
         dataset_slug = dataset.replace("-", "_")
-        BASE_URL = {
-            "localhost:8001": "localhost:3000",
-            "development.api.basedosdados.org": "development.basedosdados.org",
-            "staging.api.basedosdados.org": "homologation.basedosdados.org",
-            "api.basedosdados.org": "basedosdados.org",
-        }
-        redirect_domain = BASE_URL[domain]
+        redirect_domain = URL_MAPPING[domain]
 
         try:
             redirect_url = CloudTable.objects.filter(gcp_dataset_id=dataset_slug).first()
@@ -31,7 +32,6 @@ class DatasetRedirectView(View):
                 raise CloudTable.DoesNotExist
             redirect_url = f"http://{redirect_domain}/dataset/{str(redirect_url.table.dataset.id)}"
         except CloudTable.DoesNotExist:
-            # não tem cloud table, procura pelo nome do dataset
             try:
                 new_ds = Dataset.objects.filter(slug__icontains=dataset_slug).first()
                 redirect_url = f"http://{redirect_domain}/dataset/{str(new_ds.id)}"
