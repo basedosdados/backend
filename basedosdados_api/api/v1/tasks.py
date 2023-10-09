21be2646eb6566c1c4247ef3ff4bbda50858bc47
Vinicius Aguiar <vncsna@gmail.com>
2023-10-09 16:18:46 -0300

    fix: update import of tasks utils

--- basedosdados_api/api/v1/tasks.py
+++ basedosdados_api/api/v1/tasks.py
@@ -3,14 +3,14 @@ from huey import crontab
 from huey.contrib.djhuey import periodic_task
 from loguru import logger
 
-from basedosdados_api.api.v1.admin import _update_table_metadata
+from basedosdados_api.api.v1.admin import update_table_metadata
 
 
 @periodic_task(crontab(minute="*/10"))
-def every_ten_mins():
+def every_ten_mins_task():
     logger.info("Am I alive between these periods?")
 
 
 @periodic_task(crontab(day_of_week="0", hour="0", minute="0"))
-def update_table_metadata():
-    _update_table_metadata()
+def update_table_metadata_task():
+    update_table_metadata()
