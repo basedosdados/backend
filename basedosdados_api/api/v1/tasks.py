a263437b126f825441c94ad659b135efd4c95b35
Vinicius Aguiar <vncsna@gmail.com>
2023-10-16 17:10:28 -0300

    fix: enable huey tasks logging

--- basedosdados_api/api/v1/tasks.py
+++ basedosdados_api/api/v1/tasks.py
@@ -4,7 +4,12 @@ from huey.contrib.djhuey import periodic_task
 from loguru import logger
 
 from basedosdados_api.api.v1.admin import update_table_metadata
-from basedosdados_api.utils import prod_task
+from basedosdados_api.logger import setup_logger
+from basedosdados_api.utils import is_remote, prod_task
+
+level = "INFO" if is_remote() else "DEBUG"
+serialize = True if is_remote() else False
+setup_logger(level=level, serialize=serialize)
 
 
 @periodic_task(crontab(minute="*/10"))
