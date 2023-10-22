2ec4630bf5fc8bddcf1be9e6c55e0822f41d14ad
Vinicius Aguiar <vncsna@gmail.com>
2023-10-22 17:09:43 -0300

    fix: remove missing schema view

--- bd_api/apps/schemas/urls.py
+++ bd_api/apps/schemas/urls.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 
-from bd_api.apps.schemas.views import (
-    BdsSpatialCoverageTreeSchemaView,
-    ColumnSchemaView,
-    DatasetSchemaView,
-    TableSchemaView,
-)
+from bd_api.apps.schemas.views import ColumnSchemaView, DatasetSchemaView, TableSchemaView
 
 urlpatterns = [
     path("bd_dataset_schema/", DatasetSchemaView.as_view()),
     path("bd_bdm_table_schema/", TableSchemaView.as_view()),
     path("bd_bdm_columns_schema/", ColumnSchemaView.as_view()),
-    path("bd_spatial_coverage_tree/", BdsSpatialCoverageTreeSchemaView.as_view()),
 ]
--- bd_api/apps/schemas/views.py
+++ bd_api/apps/schemas/views.py
@@ -1,37 +1,30 @@
 # -*- coding: utf-8 -*-
 
-import json
+from json import loads
 from pathlib import Path
 
 from django.http.response import JsonResponse
 from django.views import View
 
 
+def load(filename: str) -> dict:
+    filepath = Path(__file__).parent / "repository" / f"{filename}.json"
+    return loads(filepath.read_text())
+
+
 class DatasetSchemaView(View):
     def get(self, request):
-        with open(Path.cwd().parent / "bd_api/schemas/repository/dataset_schema.json") as f:
-            schema = json.load(f)
+        schema = load("dataset_schema")
         return JsonResponse(schema, safe=False)
 
 
 class TableSchemaView(View):
     def get(self, request):
-        with open(Path.cwd().parent / "bd_api/schemas/repository/table_schema.json") as f:
-            schema = json.load(f)
+        schema = load("table_schema")
         return JsonResponse(schema, safe=False)
 
 
 class ColumnSchemaView(View):
     def get(self, request):
-        with open(Path.cwd().parent / "bd_api/schemas/repository/columns_schema.json") as f:
-            schema = json.load(f)
-        return JsonResponse(schema, safe=False)
-
-
-class BdsSpatialCoverageTreeSchemaView(View):
-    def get(self, request):
-        with open(
-            Path.cwd().parent / "bd_api/schemas/repository/bd_spatial_coverage_tree.json"
-        ) as f:
-            schema = json.load(f)
+        schema = load("columns_schema")
         return JsonResponse(schema, safe=False)
