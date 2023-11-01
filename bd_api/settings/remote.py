6c77550fbe1e55c6808343f7c2d745d3a0cd49f5
Vinicius Aguiar <vncsna@gmail.com>
2023-10-31 22:00:30 -0300

    chore: rename getenv utility to getenvp

--- bd_api/settings/remote.py
+++ bd_api/settings/remote.py
@@ -7,13 +7,13 @@ from google.oauth2 import service_account
 
 from bd_api.custom.logger import setup_logger
 from bd_api.settings.base import *  # noqa
-from bd_api.utils import getadmins, getenv
+from bd_api.utils import getadmins, getenvp
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = getenvp("DJANGO_SECRET_KEY")
 
 # CSRF
 # https://docs.djangoproject.com/en/4.2/ref/csrf/
@@ -32,11 +32,11 @@ STATIC_URL = "static/"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": getenv("DB_NAME"),
-        "USER": getenv("DB_USER"),
-        "PASSWORD": getenv("DB_PASSWORD"),
-        "HOST": getenv("DB_HOST"),
-        "PORT": getenv("DB_PORT"),
+        "NAME": getenvp("DB_NAME"),
+        "USER": getenvp("DB_USER"),
+        "PASSWORD": getenvp("DB_PASSWORD"),
+        "HOST": getenvp("DB_HOST"),
+        "PORT": getenvp("DB_PORT"),
     }
 }
 
@@ -44,23 +44,23 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#email-backend
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = getenv("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = int(getenv("EMAIL_PORT", "587"))
-EMAIL_USE_TLS = bool(getenv("EMAIL_PORT", "True"))
-SERVER_EMAIL = getenv("EMAIL_HOST_USER")
-DEFAULT_FROM_EMAIL = getenv("EMAIL_HOST_USER")
+EMAIL_HOST_USER = getenvp("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = getenvp("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = int(getenvp("EMAIL_PORT", "587"))
+EMAIL_USE_TLS = bool(getenvp("EMAIL_PORT", "True"))
+SERVER_EMAIL = getenvp("EMAIL_HOST_USER")
+DEFAULT_FROM_EMAIL = getenvp("EMAIL_HOST_USER")
 
 # Logging
 setup_logger(level="INFO", serialize=True)
 
 # Google Application Credentials
-GOOGLE_APPLICATION_CREDENTIALS = getenv("GOOGLE_APPLICATION_CREDENTIALS", "")
+GOOGLE_APPLICATION_CREDENTIALS = getenvp("GOOGLE_APPLICATION_CREDENTIALS", "")
 
 # Google Cloud Storage
-GS_SERVICE_ACCOUNT = getenv("GCP_SA")
+GS_SERVICE_ACCOUNT = getenvp("GCP_SA")
 GS_CREDENTIALS = service_account.Credentials.from_service_account_info(loads(GS_SERVICE_ACCOUNT))
-GS_BUCKET_NAME = getenv("GCP_BUCKET_NAME")
+GS_BUCKET_NAME = getenvp("GCP_BUCKET_NAME")
 GS_EXPIRATION = timedelta(seconds=604800)
 STORAGES = {
     "default": {
@@ -72,9 +72,9 @@ STORAGES = {
 }
 
 # Stripe
-STRIPE_LIVE_MODE = bool(getenv("STRIPE_LIVE_MODE"))
-STRIPE_LIVE_SECRET_KEY = getenv("STRIPE_LIVE_SECRET_KEY")
-STRIPE_TEST_SECRET_KEY = getenv("STRIPE_TEST_SECRET_KEY")
-DJSTRIPE_WEBHOOK_SECRET = getenv("DJSTRIPE_WEBHOOK_SECRET")
+STRIPE_LIVE_MODE = bool(getenvp("STRIPE_LIVE_MODE"))
+STRIPE_LIVE_SECRET_KEY = getenvp("STRIPE_LIVE_SECRET_KEY")
+STRIPE_TEST_SECRET_KEY = getenvp("STRIPE_TEST_SECRET_KEY")
+DJSTRIPE_WEBHOOK_SECRET = getenvp("DJSTRIPE_WEBHOOK_SECRET")
 DJSTRIPE_USE_NATIVE_JSONFIELD = True
 DJSTRIPE_FOREIGN_KEY_TO_FIELD = "id"
