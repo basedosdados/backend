3168989c559aab4111917949f5eb772b93489d13
Vinicius <vncsna@gmail.com>
2023-11-21 21:05:55 -0300

    feat: rebuild search index at sundays

--- bd_api/apps/api/v1/tasks.py
+++ bd_api/apps/api/v1/tasks.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.core.management import call_command
 from huey import crontab
 from huey.contrib.djhuey import periodic_task
 
@@ -20,3 +21,9 @@ def healthcheck_task():
 @periodic_task(crontab(day_of_week="0", hour="0", minute="0"))
 def update_table_metadata_task():
     update_table_metadata()
+
+
+@prod_task
+@periodic_task(crontab(day_of_week="0", hour="2", minute="0"))
+def rebuild_search_index_task():
+    call_command("rebuild_index", interactive=False, batchsize=100, workers=4)
