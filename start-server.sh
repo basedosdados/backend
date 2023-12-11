767393f514f51446d21f1b2d4c03ab7b7d9c52cc
Vinicius <vncsna@gmail.com>
2023-12-11 20:21:57 -0300

    fix: remove quiet parameter from huey call

--- start-server.sh
+++ start-server.sh
@@ -5,5 +5,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
   (cd /app; python manage.py createsuperuser --no-input)
 fi
-(cd /app; python manage.py run_huey --quiet &)
+(cd /app; python manage.py run_huey &)
 (cd /app; gunicorn bd_api.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3 --timeout 180) & nginx -g "daemon off;"
