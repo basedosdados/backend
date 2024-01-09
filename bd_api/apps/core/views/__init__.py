ef05a843f1e6b3ab02082f1e4e2b60e151c86592
Vinicius Aguiar <vncsna@gmail.com>
2024-01-08 22:31:52 -0400

    chore: purge dataset raw crud (#541)

--- bd_api/apps/core/views/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .dataset_redirect import DatasetRedirectView  # noqa: F401
-from .dataset_views import DatasetCreateView, DatasetDeleteView, DatasetUpdateView  # noqa: F401
