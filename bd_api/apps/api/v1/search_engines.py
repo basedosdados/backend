acd84dd99a85780f2c13ae3fd7c09cc90acca684
Vinicius Aguiar <vncsna@gmail.com>
2024-01-08 22:41:39 -0400

    feat: split indexing function of search (#537)

--- bd_api/apps/api/v1/search_engines.py
+++ bd_api/apps/api/v1/search_engines.py
@@ -3,7 +3,6 @@
 Based on the following docs:
 https://gregbrown.co/code/haystack-character-folding/
 https://github.com/mounirmesselmeni/MounirMesselmeni.github.io/tree/master/2015/12/13/enable-asciifolding-in-elasticsearchhaystack
-They are outdated, as are docs from 2015
 """
 
 from abc import ABCMeta
@@ -12,9 +11,9 @@ from haystack import indexes  # noqa: F401
 from haystack.backends import elasticsearch7_backend as es_backend
 
 
-class AsciifoldingElasticBackend(es_backend.Elasticsearch7SearchBackend, metaclass=ABCMeta):
+class ASCIIFoldingElasticBackend(es_backend.Elasticsearch7SearchBackend, metaclass=ABCMeta):
     def __init__(self, *args, **kwargs):
-        super(AsciifoldingElasticBackend, self).__init__(*args, **kwargs)
+        super(ASCIIFoldingElasticBackend, self).__init__(*args, **kwargs)
         analyzer = {
             "ascii_ngram_analyser": {
                 "type": "custom",
@@ -65,7 +64,7 @@ class AsciifoldingElasticBackend(es_backend.Elasticsearch7SearchBackend, metacla
         self.DEFAULT_SETTINGS["settings"]["analysis"]["filter"] = filter
 
     def build_schema(self, fields):
-        content_field_name, mapping = super(AsciifoldingElasticBackend, self).build_schema(fields)
+        content_field_name, mapping = super(ASCIIFoldingElasticBackend, self).build_schema(fields)
 
         for field_name, field_class in fields.items():
             field_mapping = mapping[field_class.index_fieldname]
@@ -106,4 +105,4 @@ class AsciifoldingElasticBackend(es_backend.Elasticsearch7SearchBackend, metacla
 
 
 class AsciifoldingElasticSearchEngine(es_backend.Elasticsearch7SearchEngine):
-    backend = AsciifoldingElasticBackend
+    backend = ASCIIFoldingElasticBackend
