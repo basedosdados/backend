a60824dddc8f029eb5eca8f73abd05b4305f6ee2
Vinicius <vncsna@gmail.com>
2024-05-06 21:32:31 -0300

    chore: add graphql standalone endpoint

--- bd_api/apps/api/v1/urls.py
+++ bd_api/apps/api/v1/urls.py
@@ -8,8 +8,8 @@ from bd_api.apps.api.v1.search_views import DatasetSearchView
 from bd_api.apps.api.v1.views import DatasetRedirectView
 
 
-def redirect_to_v1(request):
-    return HttpResponseRedirect("/api/v1/graphql")
+def redirect_to_graphql(request):
+    return HttpResponseRedirect("graphql")
 
 
 def graphql_view():
@@ -17,9 +17,10 @@ def graphql_view():
 
 
 urlpatterns = [
-    path("api/", redirect_to_v1),
-    path("api/v1/", redirect_to_v1),
+    path("api/", redirect_to_graphql),
+    path("api/v1/", redirect_to_graphql),
     path("api/v1/graphql", graphql_view()),
+    path("graphql", graphql_view()),
     path("search/", DatasetSearchView.as_view()),
     path("dataset/", DatasetRedirectView.as_view()),
     path("dataset_redirect/", DatasetRedirectView.as_view()),
