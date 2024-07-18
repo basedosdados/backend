614e06afe886c5b4e2adce1f045f655b007344db
Jhony Lucas <jhonyjl37@gmail.com>
2024-07-18 17:20:42 -0300

    feat: populate staging and dev with prod data (#636)

--- backend/apps/core/admin.py
+++ backend/apps/core/admin.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
-from django.contrib import admin  # noqa
+from django.contrib import admin
 
-# Register your models here.
+from .models import TaskExecution
+
+
+class TaskExecutionAdmin(admin.ModelAdmin):
+    list_display = ["task_name", "execution_time", "status", "duration"]
+    readonly_fields = ["task_name", "execution_time", "duration", "status", "result", "error"]
+
+    def has_add_permission(self, request):
+        return False
+
+
+admin.site.register(TaskExecution, TaskExecutionAdmin)
---
+++ backend/custom/task_decorators.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from functools import wraps
+
+from backend.apps.core.models import TaskExecution
+
+
+def log_task_execution(task_name):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            status = "success"
+            result = None
+            error = None
+            start_time = datetime.now()
+
+            try:
+                result = func(*args, **kwargs)
+            except Exception as e:
+                status = "falied"
+                error = str(e)
+            finally:
+                end_time = datetime.now()
+                duration = (end_time - start_time).total_seconds()
+
+                TaskExecution.objects.create(
+                    task_name=task_name,
+                    status=status,
+                    result=result,
+                    error=error,
+                    execution_time=start_time,
+                    duration=duration,
+                )
+            return result
+
+        return wrapper
+
+    return decorator
