43aa676f60fac4dc0a022a5d4d11a144ba568d18
Jhony Lucas <jhonyjl37@gmail.com>
2024-07-19 10:58:10 -0300

    fix: sync database with prod on task execution creation

--- backend/apps/core/signals.py
+++ backend/apps/core/signals.py
@@ -2,11 +2,13 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from backend.custom.environment import is_prd
+
 from .models import TaskExecution
 from .tasks import sync_database_with_prod
 
 
 @receiver(post_save, sender=TaskExecution)
 def task_execution_post_save(sender, instance, created, **kwargs):
-    if created:
+    if created and not is_prd():
         sync_database_with_prod()
