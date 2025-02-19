7b0a234ecd62c6228c62048a401b01c948f207ae
Ricardo Dahis <rdahis@gmail.com>
2025-02-19 16:36:11 +1100

    chore: change name of environment variables

--- backend/apps/data_api/decorators.py
+++ backend/apps/data_api/decorators.py
@@ -10,10 +10,10 @@ def cloud_function_only(view_func):
     @wraps(view_func)
     def wrapped_view(view_instance, request, *args, **kwargs):
         # Get the Cloud Function's secret key from environment variables
-        cloud_function_key = os.getenv("CLOUD_FUNCTION_KEY")
+        cloud_function_key = os.getenv("DATA_API_CLOUD_FUNCTION")
 
         # Get the authorization header
-        auth_header = request.headers.get("X-Cloud-Function-Key")
+        auth_header = request.headers.get("X-Data-API-Cloud-Function")
 
         if not auth_header or auth_header != cloud_function_key:
             return JsonResponse({"error": "Unauthorized access", "success": False}, status=403)
@@ -27,10 +27,10 @@ def stripe_webhook_only(view_func):
     @wraps(view_func)
     def wrapped_view(view_instance, request, *args, **kwargs):
         # Get the Stripe webhook secret from environment variables
-        stripe_webhook_secret = os.getenv("STRIPE_WEBHOOK_SECRET")
+        stripe_webhook_secret = os.getenv("DATA_API_STRIPE_WEBHOOK")
 
         # Get the Stripe signature header
-        stripe_signature = request.headers.get("Stripe-Signature")
+        stripe_signature = request.headers.get("X-Data-API-Stripe-Signature")
 
         if not stripe_signature:
             return JsonResponse({"error": "Missing Stripe signature", "success": False}, status=403)
