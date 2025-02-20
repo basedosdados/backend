3523e4b42c2b21728f2aafbb608080a0cbbf6b89
isabelmeister <beelsdelonge@gmail.com>
2025-02-19 22:06:13 -0300

    fix: adjust clean_database from populate.py

--- backend/apps/core/management/commands/populate.py
+++ backend/apps/core/management/commands/populate.py
@@ -192,21 +192,36 @@ class Command(BaseCommand):
         """
         Clean database
         """
+        
         for model in tqdm(_models, desc="Set foreign keys to null"):
             foreign_keys = [
                 field
                 for field in model._meta.get_fields()
                 if isinstance(field, models.ForeignKey) and field.null is True
             ]
-
+            
             if foreign_keys:
                 field_names = [field.name for field in foreign_keys]
-                print(f"FOREIGN_KEYS Model Object to be cleaned: {model.objects}\n{'#' *15}")
                 model.objects.update(**{field_name: None for field_name in field_names})
 
-        for model in tqdm(_models, desc="Cleaning database"):
-            with transaction.atomic():
-                model.objects.all().delete()
+        models_to_delete = tqdm(_models, desc="Cleaning database")
+        
+        while models_to_delete:
+            
+            try:
+
+                with transaction.atomic():
+                    model.objects.all().delete()
+                
+                models_to_delete.remove(model)
+
+                print(f"Ainda faltam {len(models_to_delete)} para serem excluidos")
+
+            except Exception as error:
+                print(
+                    f"Falha ao excluir {model}\nFK error? :{error}\n{'#' * 30}"
+                    )
+                pass
 
     def create_instance(self, model, item):
         payload = {}
