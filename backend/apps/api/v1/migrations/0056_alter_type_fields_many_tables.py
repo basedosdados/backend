9c5227b4f42bb037c0261ee0e8036c2f0927de44
isabelmeister <beelsdelonge@gmail.com>
2025-03-03 18:41:45 -0300

    adjust migration order

--- backend/apps/api/v1/migrations/0056_alter_type_fields_many_tables.py
+++ backend/apps/api/v1/migrations/0056_alter_type_fields_many_tables.py
@@ -494,7 +494,7 @@ def alter_columns_of_update(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('v1', '0053_rename_required_requires'),
+        ('v1', '0055_alter_organization_area.py'),
     ]
 
     operations = [
