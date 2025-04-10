5fd81b31c3989e952aabd79e14a13cf4434d466c
Fred Israel <fredpublico@gmail.com>
2025-04-10 12:27:12 -0300

    Fixing stuff and adding todos

--- backend/apps/chatbot/urls.py
+++ backend/apps/chatbot/urls.py
@@ -9,24 +9,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path(
-        "chatbot/threads/",
-        ChatbotThreadListView.as_view(),
-        name="chatbot_threads",
-    ),
-    path(
-        "chatbot/threads/<uuid:thread_id>/",
-        ThreadDetailView.as_view(),
-        name="chatbot_thread",
-    ),
-    path(
-        "chatbot/threads/<uuid:thread_id>/message",
-        MessageView.as_view(),
-        name="chatbot_thread",
-    ),
-    path(
-        "chatbot/message-pairs/<uuid:message_pair_id>/feedback",
-        FeedbackView.as_view(),
-        name="chatbot_message_pair_feedback",
-    )
+    path( "chatbot/threads/", ChatbotThreadListView.as_view(),),
+    path( "chatbot/threads/<uuid:thread_id>/", ThreadDetailView.as_view(),),
+    path( "chatbot/threads/<uuid:thread_id>/message", MessageView.as_view(),),
+    path( "chatbot/message-pairs/<uuid:message_pair_id>/feedback", FeedbackView.as_view(),)
 ]
