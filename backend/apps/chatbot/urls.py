ba17e1c46ee5276609c65fadb84d9fbbde1a865c
vrtornisiello <vrtornisiello@gmail.com>
2025-04-14 17:02:10 -0300

    renamed `ChatbotThreadListView` to `ThreadListView` and created `CheckpointView` for deleting checkpoints

--- backend/apps/chatbot/urls.py
+++ backend/apps/chatbot/urls.py
@@ -2,15 +2,17 @@
 from django.urls import path
 
 from .views import (
-    ChatbotThreadListView,
+    ThreadListView,
     ThreadDetailView,
     MessageView,
     FeedbackView,
+    CheckpointView
 )
 
 urlpatterns = [
-    path( "chatbot/threads/", ChatbotThreadListView.as_view(),),
-    path( "chatbot/threads/<uuid:thread_id>/", ThreadDetailView.as_view(),),
-    path( "chatbot/threads/<uuid:thread_id>/message", MessageView.as_view(),),
-    path( "chatbot/message-pairs/<uuid:message_pair_id>/feedback", FeedbackView.as_view(),)
+    path("chatbot/threads/", ThreadListView.as_view()),
+    path("chatbot/threads/<uuid:thread_id>/", ThreadDetailView.as_view()),
+    path("chatbot/threads/<uuid:thread_id>/message", MessageView.as_view()),
+    path("chatbot/message-pairs/<uuid:message_pair_id>/feedback", FeedbackView.as_view()),
+    path("chatbot/checkpoints/<uuid:thread_id>/", CheckpointView.as_view())
 ]
