fafb9de419d54bcf22d0c112d9be13401aff4e5f
vrtornisiello <vrtornisiello@gmail.com>
2025-04-22 17:20:10 -0300

    reminder for custom authentication rules

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -84,6 +84,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    # "USER_AUTHENTICATION_RULE": "backend.apps.chatbot.authentication.user_authentication_rule", <- for custom authentication rules  # noqa: E501
 }
 
 MIDDLEWARE = [
