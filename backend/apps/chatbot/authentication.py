6484cde23475068d1c5ab6e4400bf8da05104c38
vrtornisiello <vrtornisiello@gmail.com>
2025-05-09 17:18:58 -0300

    fixed authentication rule (when a user is not found in db, django simple jwt returns `None`, so it has no `has_chatbot_access` attribute

--- backend/apps/chatbot/authentication.py
+++ backend/apps/chatbot/authentication.py
@@ -3,4 +3,6 @@ from backend.apps.account.models import Account
 
 
 def authentication_rule(user: Account) -> bool:
-    return user.has_chatbot_access
+    if user is not None:
+        return user.has_chatbot_access
+    return False
