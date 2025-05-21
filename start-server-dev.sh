c13c26252d6809b20ad477bbcaf0291943b5f2d2
vrtornisiello <vrtornisiello@gmail.com>
2025-05-21 17:32:25 -0300

    edited server starting scripts

--- start-server-dev.sh
+++ start-server-dev.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
-# start-server.sh
+# start-server-dev.sh
 echo "> Making migrations"
 (cd /app; python manage.py makemigrations)
+
 echo "> Migrating"
 (cd /app; python manage.py migrate)
+
 echo "> Installing debugpy"
 pip install debugpy
+
 echo "> Creating superuser"
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-(cd /app; python manage.py createsuperuser --no-input)
+    (cd /app; python manage.py createsuperuser --no-input)
 fi
+
 echo "> Running Huey"
 (cd /app; python manage.py run_huey &)
+
+# Start server in development mode with django
 echo "> Running server in development mode"
-# Start the server in development mode with django
 (cd /app; python -m debugpy --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8000)
