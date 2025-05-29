6008cb87f63cec2243198a899dcffec356cbe4e5
Ricardo Dahis <rdahis@gmail.com>
2025-05-29 16:05:23 +1000

    apps.py so chatbot shows in admin

---
+++ backend/apps/chatbot/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class ChatbotConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "backend.apps.chatbot"
+    verbose_name = "Chatbot"
