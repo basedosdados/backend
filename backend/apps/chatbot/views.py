dad3e670e91b0a148a764a50bc2ef47b410f60b1
vrtornisiello <vrtornisiello@gmail.com>
2025-07-18 17:33:44 -0300

    add streaming support

--- backend/apps/chatbot/views.py
+++ backend/apps/chatbot/views.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
+import json
 import os
 import uuid
 from contextlib import contextmanager
 from functools import cache
-from typing import Type, TypeVar
+from typing import Any, Iterator, Type, TypedDict, TypeVar
 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, StreamingHttpResponse
 from langchain.chat_models import init_chat_model
 from langchain_google_vertexai import VertexAIEmbeddings
 from langchain_postgres import PGVector
 from langgraph.checkpoint.postgres import PostgresSaver
+from loguru import logger
 from rest_framework import exceptions
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
@@ -28,78 +30,22 @@ from backend.apps.chatbot.serializers import (
     ThreadSerializer,
     UserMessageSerializer,
 )
-from chatbot.assistants import SQLAssistant, SQLAssistantMessage
+from backend.apps.chatbot.utils.stream import Step, process_chunk
+from chatbot.assistants import SQLAssistant, format_sql_agent_response
 from chatbot.formatters import SQLPromptFormatter
 
 ModelSerializer = TypeVar("ModelSerializer", bound=Serializer)
+
 
 # Model name/URI. Refer to the LangChain docs for valid names/URIs
 # https://python.langchain.com/api_reference/langchain/chat_models/langchain.chat_models.base.init_chat_model.html
 MODEL_URI = os.environ["MODEL_URI"]
 
 
-@cache
-def _get_feedback_sender() -> LangSmithFeedbackSender:
-    return LangSmithFeedbackSender()
-
-
-@cache
-def _get_context_provider(connection: str) -> PostgresContextProvider:
-    bq_billing_project = os.environ["BILLING_PROJECT_ID"]
-    bq_query_project = os.environ["QUERY_PROJECT_ID"]
-
-    embedding_model = os.getenv("EMBEDDING_MODEL")
-    pgvector_collection = os.getenv("PGVECTOR_COLLECTION")
-
-    if embedding_model and pgvector_collection:
-        embeddings = VertexAIEmbeddings(embedding_model)
-
-        vector_store = PGVector(
-            embeddings=embeddings,
-            connection=connection,
-            collection_name=pgvector_collection,
-            use_jsonb=True,
-        )
-    else:
-        vector_store = None
-
-    context_provider = PostgresContextProvider(
-        billing_project=bq_billing_project,
-        query_project=bq_query_project,
-        metadata_vector_store=vector_store,
-        top_k=5,
-    )
-
-    return context_provider
-
-
-@contextmanager
-def _get_sql_assistant():
-    db_host = os.environ["DB_HOST"]
-    db_port = os.environ["DB_PORT"]
-    db_name = os.environ["DB_NAME"]
-    db_user = os.environ["DB_USER"]
-    db_password = os.environ["DB_PASSWORD"]
-
-    conn = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-
-    context_provider = _get_context_provider(conn)
-
-    prompt_formatter = SQLPromptFormatter(vector_store=None)
-
-    with PostgresSaver.from_conn_string(conn) as checkpointer:
-        checkpointer.setup()
-
-        model = init_chat_model(MODEL_URI, temperature=0)
-
-        assistant = SQLAssistant(
-            model=model,
-            context_provider=context_provider,
-            prompt_formatter=prompt_formatter,
-            checkpointer=checkpointer,
-        )
-
-        yield assistant
+class ConfigDict(TypedDict):
+    run_id: str
+    recursion_limit: int
+    configurable: dict[str, Any]
 
 
 class ThreadListView(APIView):
@@ -210,42 +156,34 @@ class MessageListView(APIView):
         Returns:
             JsonResponse: A JSON response with the serialized message pair object.
         """
+        thread = _get_thread_by_id(thread_id)
+
         run_id = str(uuid.uuid4())
 
-        config = {
-            "run_id": run_id,
-            "recursion_limit": 32,
-            "configurable": {
-                "thread_id": str(thread_id),
+        config = ConfigDict(
+            run_id=run_id,
+            recursion_limit=32,
+            configurable={
+                "thread_id": str(thread.id),
             },
-        }
+        )
 
         serializer = _validate(request, UserMessageSerializer)
 
-        user_message_content = serializer.validated_data["content"]
+        message = serializer.validated_data["content"]
 
-        with _get_sql_assistant() as assistant:
-            assistant_response: SQLAssistantMessage = assistant.invoke(
-                message=user_message_content,
+        return StreamingHttpResponse(
+            _stream_sql_assistant_response(
+                message=message,
                 config=config,
-                rewrite_query=True,
-            )
-
-        thread = _get_thread_by_id(thread_id)
-
-        # TODO (nice to have): stream results
-        message_pair = MessagePair.objects.create(
-            id=assistant_response.id,
-            thread=thread,
-            model_uri=MODEL_URI,
-            user_message=user_message_content,
-            assistant_message=assistant_response.content,
-            generated_queries=assistant_response.sql_queries,
+                thread=thread,
+            ),
+            headers={
+                "Cache-Control": "no-cache",
+                "X-Accel-Buffering": "no",
+            },
+            content_type="text/event-stream",
         )
-
-        serializer = MessagePairSerializer(message_pair)
-
-        return JsonResponse(serializer.data, status=201)
 
 
 class FeedbackListView(APIView):
@@ -284,6 +222,179 @@ class FeedbackListView(APIView):
         status = 201 if created else 200
 
         return JsonResponse(serializer.data, status=status)
+
+
+@cache
+def _get_feedback_sender() -> LangSmithFeedbackSender:
+    """Provide a `LangSmithFeedbackSender` feedback sender.
+
+    Returns:
+        LangSmithFeedbackSender: An instance of `LangSmithFeedbackSender`.
+    """
+    return LangSmithFeedbackSender()
+
+
+@cache
+def _get_context_provider(connection: str) -> PostgresContextProvider:
+    """Provide a configured `PostgresContextProvider` context provider.
+
+    Args:
+        connection (str): A vector database connection for providing few-shot examples.
+
+    Returns:
+        PostgresContextProvider: An instance of `PostgresContextProvider`.
+    """
+    bq_billing_project = os.environ["BILLING_PROJECT_ID"]
+    bq_query_project = os.environ["QUERY_PROJECT_ID"]
+
+    embedding_model = os.getenv("EMBEDDING_MODEL")
+    pgvector_collection = os.getenv("PGVECTOR_COLLECTION")
+
+    if embedding_model and pgvector_collection:
+        embeddings = VertexAIEmbeddings(embedding_model)
+
+        vector_store = PGVector(
+            embeddings=embeddings,
+            connection=connection,
+            collection_name=pgvector_collection,
+            use_jsonb=True,
+        )
+    else:
+        vector_store = None
+
+    context_provider = PostgresContextProvider(
+        billing_project=bq_billing_project,
+        query_project=bq_query_project,
+        metadata_vector_store=vector_store,
+        top_k=5,
+    )
+
+    return context_provider
+
+
+@contextmanager
+def _get_sql_assistant():
+    """Provide a configured `SQLAssistant`.
+
+    Yields:
+        Iterator[SQLAssistant]: An instance of `SQLAssistant`.
+    """
+    db_host = os.environ["DB_HOST"]
+    db_port = os.environ["DB_PORT"]
+    db_name = os.environ["DB_NAME"]
+    db_user = os.environ["DB_USER"]
+    db_password = os.environ["DB_PASSWORD"]
+
+    conn = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+    context_provider = _get_context_provider(conn)
+
+    prompt_formatter = SQLPromptFormatter(vector_store=None)
+
+    with PostgresSaver.from_conn_string(conn) as checkpointer:
+        checkpointer.setup()
+
+        model = init_chat_model(MODEL_URI, temperature=0)
+
+        assistant = SQLAssistant(
+            model=model,
+            context_provider=context_provider,
+            prompt_formatter=prompt_formatter,
+            checkpointer=checkpointer,
+        )
+
+        yield assistant
+
+
+def _stream_sql_assistant_response(
+    message: str, config: ConfigDict, thread: Thread
+) -> Iterator[str]:
+    """Stream SQLAssistant's execution progress.
+
+    Args:
+        message (str): User's input message.
+        config (ConfigDict): Configuration for the assistant's execution.
+        thread (Thread): Unique identifier for the conversation thread.
+
+    Yields:
+        Iterator[str]: JSON string containing the streaming status and the current step data.
+    """
+    steps = []
+    last_chunk = None
+
+    try:
+        logger.info("Calling SQLAssistant...")
+        with _get_sql_assistant() as assistant:
+            for mode, chunk in assistant.stream(
+                message=message,
+                config=config,
+                stream_mode=["updates", "values"],
+                rewrite_query=True,
+            ):
+                last_chunk = chunk
+
+                # Skip "values" chunks during streaming. We only need the final one at the end,
+                # as it contains the SQL Agent's final state.
+                if mode == "values":
+                    continue
+
+                processed_results = process_chunk(chunk)
+
+                if processed_results is None:
+                    continue
+
+                label, content = processed_results
+
+                step = Step(label=label, content=content)
+                steps.append(step.model_dump())
+
+                yield json.dumps({"status": "running", "data": step.model_dump_json()}) + "\n\n"
+
+        # The last chunk represents the SQLAgent's final state,
+        # so we format it as the final response for the user.
+        response = format_sql_agent_response(last_chunk)
+        response["error_message"] = None
+    except Exception:
+        logger.exception(f"Error responding message {config['run_id']}:")
+        response = {
+            "error_message": (
+                "Ops, algo deu errado! Ocorreu um erro inesperado. Por favor, tente novamente. "
+                "Se o problema persistir, avise-nos. Obrigado pela paciência!"
+            ),
+            "content": None,
+            "sql_queries": None,
+        }
+
+    logger.success("SQLAssistant called successfully")
+
+    response["id"] = config["run_id"]
+
+    message_pair = MessagePair.objects.create(
+        id=response["id"],
+        thread=thread,
+        model_uri=MODEL_URI,
+        user_message=message,
+        assistant_message=response["content"],
+        error_message=response["error_message"],
+        generated_queries=response["sql_queries"],
+        steps=steps,
+    )
+
+    yield (
+        json.dumps(
+            {
+                "status": "complete",
+                "data": {
+                    "id": message_pair.id,
+                    "user_message": message_pair.user_message,
+                    "assistant_message": message_pair.assistant_message,
+                    "error_message": message_pair.error_message,
+                    "generated_queries": message_pair.generated_queries,
+                },
+            }
+        )
+        + "\n\n"
+    )
 
 
 def _get_thread_by_id(thread_id: uuid.UUID) -> Thread:
