98f3fbab5c96edc9e0b75b35dc3f113709370256
Winzen <luiz.sinx02@gmail.com>
2026-02-04 04:30:03 -0300

    feat: disable_unhealthy_flow_schedules

---
+++ backend/apps/core/management/commands/_disable_unhealthy_flow_schedules/models.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from typing import TYPE_CHECKING, Optional
+
+from .constants import Constants
+from .datetime_utils import parse_datetime
+
+if TYPE_CHECKING:
+    from .service import FlowService
+
+
+class Task:
+    def __init__(self, id: str, name: str):
+        self.id = id
+        self.name = name
+
+
+class TaskRun:
+    def __init__(
+        self,
+        id: str,
+        state: str,
+        end_time: Optional[str],
+        state_message: str,
+        task: dict,
+    ):
+        self.id = id
+        self.state = state
+        self.end_time = end_time
+        self.state_message = state_message
+        self.task = Task(**task)
+
+
+class FlowRun:
+    def __init__(
+        self,
+        id: str,
+        name: str,
+        start_time: str,
+        state: str,
+        task_runs: list,
+    ):
+        self.id = id
+        self.name = name
+        self.start_time = parse_datetime(start_time)
+        self.state = state
+        self.task_runs = TaskRun(**task_runs[0])
+
+
+class FlowDisable:
+    def __init__(self, id: str, created: str, service: "FlowService"):
+        self.id = id
+        self.created = parse_datetime(created)
+        self.service = service
+        self.runs = self.get_runs()
+
+    def get_runs(self):
+        response = self.service.last_completed_runs_tasks(self.id)
+        return [FlowRun(**run) for run in response["flow_run"]]
+
+    def validate(self) -> bool:
+        last_run, next_last = self.runs
+        failed = Constants.FLOW_FAILED_STATE.value
+
+        dbt_failed_after_created = (
+            last_run.task_runs.task.name in Constants.TASKS_NAME_DISABLE.value
+            and last_run.start_time >= self.created
+            and last_run.state == failed
+            and last_run.task_runs.state_message not in Constants.STATE_MESSAGE_IGNORE.value
+        )
+
+        any_failed_after_created = (
+            last_run.state == failed
+            and next_last.state == failed
+            and max(last_run.start_time, next_last.start_time) >= self.created
+        )
+
+        return dbt_failed_after_created or any_failed_after_created
--- backend/apps/core/tasks.py
+++ backend/apps/core/tasks.py
@@ -46,3 +46,10 @@ def sync_database_with_prod():
     call_command("fetch_metabase")
     call_command("populate")
     return "Sincronização concluída com sucesso"
+
+
+@db_periodic_task(crontab(minute="*/20"))
+# @production_task
+def disable_unhealthy_flow_schedules():
+    """Disable unhealthy flow schedules"""
+    call_command("disable_unhealthy_flow_schedules")
