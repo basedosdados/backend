9d88c352e4063eeb42e8826ed9659a24cd74bcd8
Winzen <luiz.sinx02@gmail.com>
2026-02-04 04:30:03 -0300

    feat: disable_unhealthy_flow_schedules

---
+++ backend/apps/core/management/commands/_disable_unhealthy_flow_schedules/constants.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+
+class Querys(Enum):
+    FLOWS_FAILED_LAST_WEEK = """
+    query($since: timestamptz!) {
+    flow(
+    where: {
+    schedule: { _is_null: false }
+    is_schedule_active: {_eq: true}
+    archived: {_eq: false}
+    flow_runs: {
+        state: { _eq: "Failed" }
+        start_time: { _gte: $since }
+    }
+    }
+    ) {
+    id
+    created
+    name
+    }
+    }
+    """
+
+    LAST_COMPLETED_RUNS_TASKS = """
+    query LastTwoCompletedRunsWithTasks($flow_id: uuid!) {
+    flow_run(
+    where: {
+    flow_id: { _eq: $flow_id }
+    state: { _in: ["Success", "Failed"] }
+    }
+    order_by: { start_time: desc }
+    limit: 2
+    ) {
+    id
+    name
+    start_time
+    state
+    task_runs(
+    where: {
+    state: { _in: ["Success", "Failed"] }
+    }
+    order_by: { start_time: desc }
+    limit: 1) {
+    id
+    state
+    end_time
+    state_message
+    task {
+    id
+    name
+    }
+    }
+    }
+    }
+    """
+
+
+class Constants(Enum):
+    TASKS_NAME_DISABLE = ("run_dbt",)
+    FLOW_SUCCESS_STATE = "Success"
+    FLOW_FAILED_STATE = "Failed"
+
+    PREFECT_URL = "https://prefect.basedosdados.org/"
+    PREFECT_URL_FLOW = PREFECT_URL + "flow/"
+    PREFECT_URL_API = PREFECT_URL + "api"
+
+    DISCORD_ROLE_DADOS = "865034571469160458"
+    TEXT_FLOW_FORMAT = "- {run_name} | Last failure `{task_name}` | {link}"
+
+    STATE_MESSAGE_IGNORE = (
+        "No heartbeat detected from the remote task; marking the run as failed.",
+    )
