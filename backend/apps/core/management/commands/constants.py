49feb72365e0e909b22dd901255bc823c09dd8d9
Winzen <luiz.sinx02@gmail.com>
2026-03-07 00:43:48 -0300

    Mudar constants para xml

--- backend/apps/core/management/commands/constants.py
+++ backend/apps/core/management/commands/constants.py
@@ -5,6 +5,6 @@ from pathlib import Path
 
 class LocalPolulate(Enum):
     FOLDER = "backups"
-    FILE_NAME = "backup_prod_v1.json"
-    URL_JSON = "https://storage.googleapis.com/basedosdados-dev/backend_backup/backup_prod_v1.json"
+    FILE_NAME = "backup_prod_v1.xml"
+    URL_JSON = "https://storage.googleapis.com/basedosdados-website/dados/backup_prod_v1.xml"
     PATH_FILE = Path(FOLDER) / FILE_NAME
